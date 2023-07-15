73c21e3ca1489e2696b4fff1543c1a3ac82649c7
fractalo <42487844+fractalo@users.noreply.github.com>
2023-07-16 05:30:44 +0900

    feat: detailed message for exceptions in Lotto

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,13 +14,16 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또는 6개의 번호로 구성되어야 합니다.");
         }
 
         Set<Integer> numberSet = new HashSet<>();
         for (int number : numbers) {
-            if (!isLottoNumber(number) || !numberSet.add(number)) {
-                throw new IllegalArgumentException();
+            if (!isLottoNumber(number)) {
+                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+            if (!numberSet.add(number)) {
+                throw new IllegalArgumentException("로또의 번호들은 중복되면 안됩니다.");
             }
         }
     }
