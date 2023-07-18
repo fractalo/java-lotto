86064e632fb53d7c2c3ac3cdf5c4e8eeb5cdad60
fractalo <42487844+fractalo@users.noreply.github.com>
2023-07-19 03:12:30 +0900

    feat(lottoGame): read winning and bonus numbers

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -16,6 +16,29 @@ public class LottoGame {
             throw new IllegalArgumentException("구입 금액이 숫자가 아닙니다.");
         }
     }
+
+    private Lotto readWinningLotto() {
+        String line = prompt("당첨 번호를 입력해 주세요.");
+        List<Integer> numbers = new ArrayList<>();
+        for (String token : line.split(",")) {
+            try {
+                numbers.add(Integer.parseInt(token.trim()));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("당첨 번호가 숫자가 아닙니다.");
+            }
+        }
+        return new Lotto(numbers);
+    }
+
+    private int readBonusNumber() {
+        String line = prompt("보너스 번호를 입력해 주세요.");
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("보너스 번호가 숫자가 아닙니다.");
+        }
+    }
+
     private String prompt(String message) {
         System.out.println(message);
         String line = Console.readLine();
